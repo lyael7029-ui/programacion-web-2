af215084eb94247edef888c2152ff94521bc85f9
lyael7029-ui <lyael7029@gmail.com>
2026-04-13 22:43:29 -0600

    Add files via upload

---
+++ eventos con estructura de control/app.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector("#tituloID").textContent);
+console.log(document.querySelector("#tituloID").textContent); 
+
+const h1=document.querySelector("#tituloID");
+console.log(h1.className);
+@@ -17,4 +17,4 @@ h1.style.color="red";
+h1.style.backgroundColor="black";
+h1.style.textAlign="center";
+h1.style.padding="20px";
+h1.style.borderRadius="10px";
+h1.style.borderRadius="10px";
